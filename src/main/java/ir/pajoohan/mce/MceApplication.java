2ab773d35bf0d75673425282b3df524a96421877
Ali.Pajoohanfar <Ali.Pajoohanfar@live.com>
2023-08-31 16:36:31 +0330

    First commit. Create Master!

---
+++ src/main/java/ir/pajoohan/mce/MceApplication.java
@@ -0,0 +1,13 @@
+package ir.pajoohan.mce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/pajoohan/mce/MceApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.pajoohan.mce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
