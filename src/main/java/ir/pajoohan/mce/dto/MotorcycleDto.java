ca0ae1cffa5c908f2af7cd5dcc02848ee0754842
Ali Pajoohanfar <Ali.Pajoohanfar@live.com>
2023-11-16 13:50:33 +0330

    Add Motorcycle Dto.

---
+++ src/main/java/ir/pajoohan/mce/dto/MotorcycleDto.java
@@ -0,0 +1,32 @@
+package ir.pajoohan.mce.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Component
+public class MotorcycleDto {
+
+    private Long id;
+    private String engineMum;
+    private String chassisNum;
+    private Date numberingDate;
+    private Date exitDate;
+    private String licensePlate;
+    private String waybillNum;
+    private Date customerBuyDate;
+
+    private Long motorcycleTypeId;
+    private Long branchId;
+    private Long customerId;
+    private Long warehouseInputId;
+    private Long colorId;
+    private Long statusId;
+    private Long subBranchId;
+    private Long stateId;
+    private Long attachmentId;
+}
---
+++ src/main/java/ir/pajoohan/mce/dto/MotorcycleMapper.java
@@ -0,0 +1,34 @@
+package ir.pajoohan.mce.dto;
+
+import ir.pajoohan.mce.entity.Motorcycle;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MotorcycleMapper {
+
+    MotorcycleMapper INSTANCE = Mappers.getMapper(MotorcycleMapper.class);
+
+    @Mapping(target = "motorcycleTypeId", source = "motorcycle.motorcycleType.id")
+    @Mapping(target = "branchId", source = "motorcycle.branch.id")
+    @Mapping(target = "customerId", source = "motorcycle.customer.id")
+    @Mapping(target = "warehouseInputId", source = "motorcycle.warehouseInput.id")
+    @Mapping(target = "colorId", source = "motorcycle.color.id")
+    @Mapping(target = "statusId", source = "motorcycle.status.id")
+    @Mapping(target = "subBranchId", source = "motorcycle.subBranch.id")
+    @Mapping(target = "stateId", source = "motorcycle.state.id")
+    @Mapping(target = "attachmentId", source = "motorcycle.attachment.id")
+    MotorcycleDto MotorcycleToMotorcycleDto(Motorcycle motorcycle);
+
+    @Mapping(target = "motorcycleType.id", source = "motorcycleTypeId")
+    @Mapping(target = "branch.id", source = "branchId")
+    @Mapping(target = "customer.id", source = "customerId")
+    @Mapping(target = "warehouseInput.id", source = "warehouseInputId")
+    @Mapping(target = "color.id", source = "colorId")
+    @Mapping(target = "status.id", source = "statusId")
+    @Mapping(target = "subBranch.id", source = "subBranchId")
+    @Mapping(target = "state.id", source = "stateId")
+    @Mapping(target = "attachment.id", source = "attachmentId")
+    Motorcycle MotorcycleDtoToMotorcycle(MotorcycleDto motorcycleDto);
+}
