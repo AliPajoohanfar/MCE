1e3d2a1386aec9dfcc8aa1a75914d0137d428925
Ali Pajoohanfar <Ali.Pajoohanfar@live.com>
2023-12-01 15:09:21 +0330

    Database Readme Update.

--- src/main/resources/OtherProjectFiles/Script - Create User MCE.sql
+++ src/main/resources/OtherProjectFiles/Script - Create User MCE.sql
@@ -1,7 +1,11 @@
 -- 1 ------------------------------------
-create
-user MCE identified by mce;
+create user MCE identified by mce;
 
 -- 2 ------------------------------------
-
 GRANT DBA TO mce WITH ADMIN OPTION;
+
+-- 3 ------------------------------------
+-- Go to CLI (cmd r Bash)
+-- Change Directory to DB-Export.sql Directory.ALLOCATE
+sqlplus mce/mce
+@DB-Export.sql
