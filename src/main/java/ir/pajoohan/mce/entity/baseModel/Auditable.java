4257d1af95e0555ad31c35837da21c0a732d5d4c
Ali Pajoohanfar <Ali.Pajoohanfar@live.com>
2023-12-04 16:53:55 +0330

    Use Lombok for Auditable.java

--- src/main/java/ir/pajoohan/mce/entity/baseModel/Auditable.java
+++ src/main/java/ir/pajoohan/mce/entity/baseModel/Auditable.java
@@ -4,6 +4,8 @@ package ir.pajoohan.mce.entity.baseModel;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Temporal;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -14,7 +16,8 @@ import java.util.Date;
 
 import static jakarta.persistence.TemporalType.TIMESTAMP;
 
-
+@Setter
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class Auditable<U> {
@@ -33,39 +36,4 @@ public abstract class Auditable<U> {
     @Temporal(TIMESTAMP)
     protected Date lastModifiedDate;
 
-    /*----------------------------------------------------------------------------------------------------------------*/
-
-    public U getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(U createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-    /*----------------------------------------------------------------------------------------------------------------*/
-
-    public U getLastModifiedBy() {
-        return lastModifiedBy;
-    }
-
-    public void setLastModifiedBy(U lastModifiedBy) {
-        this.lastModifiedBy = lastModifiedBy;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-    /*----------------------------------------------------------------------------------------------------------------*/
 }
