8408bd7114abafae96bc13a6f1ef44109c97645f
Ali Pajoohanfar <Ali.Pajoohanfar@live.com>
2023-12-04 13:44:31 +0330

    Add Data time and millisecond to Entity JSON Response created and modified Date.

--- src/main/java/ir/pajoohan/mce/dto/baseDto/AuditDto.java
+++ src/main/java/ir/pajoohan/mce/dto/baseDto/AuditDto.java
@@ -1,5 +1,6 @@
 package ir.pajoohan.mce.dto.baseDto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +10,9 @@ import java.util.Date;
 @Setter
 public class AuditDto {
     private String createdBy;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss:SSS")
     private Date createdDate;
     private String lastModifiedBy;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss:SSS")
     private Date lastModifiedDate;
 }
