cf66b1c26402c53eade20059a1e0a097054083e0
Ali Pajoohanfar <Ali.Pajoohanfar@live.com>
2023-12-10 11:13:47 +0330

    Add EngineType Controller. Update EngineType Service.

---
+++ src/main/java/ir/pajoohan/mce/controller/EngineTypeRestController.java
@@ -0,0 +1,75 @@
+package ir.pajoohan.mce.controller;
+
+import ir.pajoohan.mce.dto.EngineTypeDto;
+import ir.pajoohan.mce.service.EngineTypeService;
+import ir.pajoohan.mce.service.impl.EngineTypeServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/enginetype")
+@Validated
+public class EngineTypeRestController {
+
+    EngineTypeService engineTypeService;
+
+    /**
+     * Setters
+     */
+    @Autowired
+    public void setEngineTypeService(EngineTypeServiceImpl engineTypeServiceImpl) {
+        this.engineTypeService = engineTypeServiceImpl;
+    }
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    /**
+     * Methods
+     */
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<List<EngineTypeDto>> getAll() {
+        return ResponseEntity.ok().body(engineTypeService.getAll());
+    }
+
+    @GetMapping("/{engineTypeId}")
+    @ResponseBody
+    public ResponseEntity<EngineTypeDto> get(@PathVariable("engineTypeId") Long engineTypeId) {
+        return ResponseEntity.ok().body(
+                engineTypeService.get(engineTypeId));
+    }
+
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<EngineTypeDto> insert(@RequestBody @Valid EngineTypeDto engineTypeDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(engineTypeService.save(engineTypeDto));
+    }
+
+    @PutMapping
+    @ResponseBody
+    public ResponseEntity<EngineTypeDto> update(@RequestBody @Valid EngineTypeDto engineTypeDto) {
+        return ResponseEntity.ok().body(engineTypeService.update(engineTypeDto));
+    }
+
+    @DeleteMapping("/{engineTypeId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable("engineTypeId") Long engineTypeId) {
+        engineTypeService.delete(engineTypeId);
+    }
+
+}
--- src/main/java/ir/pajoohan/mce/service/impl/EngineTypeServiceImpl.java
+++ src/main/java/ir/pajoohan/mce/service/impl/EngineTypeServiceImpl.java
@@ -1,33 +1,78 @@
 package ir.pajoohan.mce.service.impl;
 
 import ir.pajoohan.mce.dto.EngineTypeDto;
+import ir.pajoohan.mce.dto.EngineTypeMapper;
+import ir.pajoohan.mce.entity.EngineType;
+import ir.pajoohan.mce.repository.EngineTypeRepository;
 import ir.pajoohan.mce.service.EngineTypeService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Service
+@Transactional
 public class EngineTypeServiceImpl implements EngineTypeService {
+
+    EngineTypeRepository engineTypeRepository;
+
+    /**
+     * Setters
+     */
+    @Autowired
+    public void setEngineTypeRepository(EngineTypeRepository engineTypeRepository) {
+        this.engineTypeRepository = engineTypeRepository;
+    }
+
+    /*----------------------------------------------------------------------------------------------------------------*/
+
+    /**
+     * Methods
+     */
     @Override
     public List<EngineTypeDto> getAll() {
-        return null;
+        List<EngineType> engineTypeList = engineTypeRepository.findAll();
+        List<EngineTypeDto> engineTypeDtoList = new ArrayList<>();
+        for (EngineType e : engineTypeList) {
+            engineTypeDtoList.add(EngineTypeMapper.INSTANCE.engineTypeToEngineTypeDto(e));
+        }
+        return engineTypeDtoList;
     }
 
     @Override
     public EngineTypeDto get(Long id) {
-        return null;
+        EngineType engineType = engineTypeRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        return EngineTypeMapper.INSTANCE.engineTypeToEngineTypeDto(engineType);
     }
 
     @Override
     public EngineTypeDto save(EngineTypeDto engineTypeDto) {
-        return null;
+        EngineType engineType = EngineTypeMapper.INSTANCE.engineTypeDtoToEngineType(engineTypeDto);
+        engineType.setId(null);
+        EngineType engineTypeSaved = engineTypeRepository.save(engineType);
+        return EngineTypeMapper.INSTANCE.engineTypeToEngineTypeDto(engineTypeSaved);
     }
 
     @Override
     public EngineTypeDto update(EngineTypeDto engineTypeDto) {
-        return null;
+        Optional<EngineType> optionalEngineType = engineTypeRepository.findById(engineTypeDto.getId());
+        if (optionalEngineType.isPresent()) {
+            EngineType engineType = optionalEngineType.get();
+            EngineTypeMapper.INSTANCE.updateEngineTypeFromDto(engineTypeDto, engineType);
+            EngineType engineTypeSaved = engineTypeRepository.save(engineType);
+            return EngineTypeMapper.INSTANCE.engineTypeToEngineTypeDto(engineTypeSaved);
+        } else {
+            throw new EntityNotFoundException("ENGINE_TYPE with ID : '" + engineTypeDto.getId() + "' not found.");
+        }
     }
 
     @Override
     public void delete(Long id) {
-
+        EngineType engineType = engineTypeRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        engineTypeRepository.delete(engineType);
     }
 }
